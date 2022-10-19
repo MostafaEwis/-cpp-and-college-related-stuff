3ec23d9db438e3a5b18d5d7e06dbaab40e0366c4
Mostafa Ewis <64862002+MostafaEwis@users.noreply.github.com>
2022-10-19 12:15:30 +0200

    Solving a proplem for a friend, who needed to find the sigma of even numbers in the set of n of numbers.

---
+++ sumofevennums.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    long long int n = 0;
+    cin >> n;
+    int e_num;
+    if (n % 2 == 0)
+    {
+        e_num = n / 2;
+    }
+    else
+    {
+        e_num = (n - 1) / 2;
+    }
+    cout << (e_num * (e_num + 1)) << endl;
+    return 0;
+}
